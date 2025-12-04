43b92a85c4738b044f0f16646d14042f69c2fb80
priyanshu singh <priyanshu.code01@gmail.com>
2025-12-04 15:28:13 +0530

    Reverse a String

---
+++ 05_Problem.js
@@ -0,0 +1,7 @@
+// Question 5: Reverse a String
+
+function reverseString(str) {
+        return str.split('').reverse().join('');
+}
+
+console.log(reverseString("JavaScript")); // Should output: tpircSavaJ
