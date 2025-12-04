170dd94248b8a23da76a7f4f3872f0f47ea95861
priyanshu singh <priyanshu.code01@gmail.com>
2025-12-04 15:25:57 +0530

    Even or Odd?

---
+++ 02_Problem.js
@@ -0,0 +1,12 @@
+// Question 2: Even or Odd?
+
+function isEven(num) {
+  if (num % 2 === 0) {
+    return "Even";
+  } else {
+    return "Odd";
+  }
+}
+
+console.log(isEven(1));  // Odd
+console.log(isEven(4));  // Even
